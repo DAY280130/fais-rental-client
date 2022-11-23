c03203d8497ae356e3596eb2e58dc3d0fda261a5
DAY280130 <dimas.280130@gmail.com>
2022-11-23 15:49:44 +0700

    android emulator config done

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'fais-rental-client',
+  appId: 'com.fais.faisrental',
+  appName: 'Fais-Rental',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
